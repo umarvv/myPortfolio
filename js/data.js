73d95c3e94967c9d6c80b47c91925a30aea9a259
umarvv <a_scm@icloud.com>
2025-04-07 13:10:00 +0500

    first commit

---
+++ js/data.js
@@ -0,0 +1,36 @@
+export default [
+  {
+    title: "Coffee Street",
+    github: "https://github.com/umarvv/coffee-street",
+    vercel: "https://coffee-street-blond.vercel.app/",
+  },
+  {
+    title: "Akademnashr",
+    github: "https://github.com/umarvv/akademik",
+    vercel: "https://akademik-six.vercel.app/",
+  },
+
+  {
+    title: "Card INFO",
+    github: "https://github.com/umarvv/infocard",
+    vercel: "https://infocard-drab.vercel.app/",
+  },
+
+  {
+    title: "XZ",
+    github: "https://github.com/umarvv/card1",
+    vercel: "https://card1-gules.vercel.app/",
+  },
+
+  {
+    title: "Figma",
+    github: "https://github.com/umarvv/fgima",
+    vercel: "https://fgima.vercel.app/",
+  },
+
+  {
+    title: "2-oy 6-vazifa",
+    github: "https://github.com/umarvv/parfume",
+    vercel: "https://parfume-lilac.vercel.app/",
+  },
+];
